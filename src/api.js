2c981571a5961370a425a566e36d38b95bc424bb
Roman <romchikus1987@gmail.com>
2022-12-01 17:15:12 +0200

    weather app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '6a11877160mshf835789cd1b01d2p1db68djsnfa34caa24647',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '969531c482b01adbe493835643535db5';
